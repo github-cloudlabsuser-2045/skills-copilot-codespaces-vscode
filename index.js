f0db75f7e2443b215e076c66251f089be0a2e3ca
github-cloudlabsuser-2045 <169063391+github-cloudlabsuser-2045@users.noreply.github.com>
2024-06-04 18:56:56 +0000

    added data

--- index.js
+++ index.js
@@ -24,3 +24,16 @@ const sentence = "Hello, world!";
 const reversedSentence = sentence.split('').reverse().join('');
 const capitalizedSentence = reversedSentence.charAt(0).toUpperCase() + reversedSentence.slice(1);
 console.log(capitalizedSentence);
+
+const data = [
+    [
+        { name: 'John', age: 25 },
+        { name: 'Jane', age: 30 }
+    ],
+    [
+        { name: 'Bob', age: 40 }
+    ]
+];
+
+const names = data.flatMap(arr => arr.map(obj => obj.name));
+console.log(names);
